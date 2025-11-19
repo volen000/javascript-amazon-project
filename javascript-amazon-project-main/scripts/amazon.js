807f19b9a8280d9ee1790fbca965dd89f53d01fd
Volen <volentodorov00@gmail.com>
2025-11-19 13:00:01 +0200

    First code

---
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -0,0 +1,28 @@
+const products = [  {
+
+image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+ratings: {
+
+	stars:4.5,
+	count:87
+
+},
+princeCents: 1090
+
+}, 
+{
+
+image: 'images/products/intermediate-composite-basketball.jpg',
+name: ' Intermediate Size Basketballs',
+ratings: {
+
+	stars:4.5,
+	count:87
+
+},
+princeCents: 2090
+
+}];
+
+console.log(products[0]);
